68a346c4f7fc9294f919932555035f9b6ffaf251
Muhammed Shan <shanushan73011@gmail.com>
2025-09-30 14:07:49 +0530

    fix the small device design issues

--- frontend/src/components/Skeleton/chatLoadingSkeleton.tsx
+++ frontend/src/components/Skeleton/chatLoadingSkeleton.tsx
@@ -2,28 +2,29 @@ import { MessageSkeleton } from "./MessageSkeleton"
 
 export const ChatLoadingSkeleton = () => {
   return (
-    <div className="flex-1 flex flex-col h-full">
-      {/* Header Loading Skeleton */}
-      <div className="border-b border-base-300 p-4">
-        <div className="flex items-center gap-3">
-          <div className="skeleton w-10 h-10 rounded-full" />
-          <div className="flex flex-col gap-2">
-            <div className="skeleton h-4 w-24" />
-            <div className="skeleton h-3 w-16" />
-          </div>
-        </div>
-      </div>
-
-      {/* Messages Loading Skeleton */}
-      <MessageSkeleton />
-
-      {/* Input Loading Skeleton */}
-      <div className="border-t border-base-300 p-4">
-        <div className="flex items-center gap-2">
-          <div className="skeleton h-10 flex-1 rounded-md" />
-          <div className="skeleton h-10 w-10 rounded-md" />
-        </div>
+<div className="flex-1 flex flex-col h-full min-w-0">
+  {/* Header Loading Skeleton */}
+  <div className="border-b border-base-300 p-4">
+    <div className="flex items-center gap-3">
+      <div className="skeleton w-10 h-10 rounded-full" />
+      <div className="flex flex-col gap-2">
+        <div className="skeleton h-4 w-24" />
+        <div className="skeleton h-3 w-16" />
       </div>
     </div>
+  </div>
+
+  {/* Messages Loading Skeleton */}
+  <MessageSkeleton />
+
+  {/* Input Loading Skeleton */}
+  <div className="border-t border-base-300 p-4">
+    <div className="flex items-center gap-2">
+      <div className="skeleton h-10 flex-1 rounded-md" />
+      <div className="skeleton h-10 w-10 rounded-md" />
+    </div>
+  </div>
+</div>
+
   )
 }
